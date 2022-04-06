4c44367c538a137aa58bd3b2e36769632255af63
Ai-Lyn Tang <ailyntang@gmail.com>
2022-04-06 21:09:06 +1000

    Add new xcode project

---
+++ CycleTracker/CycleTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CycleTrackerApp.swift
+//  CycleTracker
+//
+//  Created by Empower on 6/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct CycleTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
